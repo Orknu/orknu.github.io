7d3b4e74f667c43480fe7a3312d2229d180620d9
Orknu <orkun@yandex.com>
2023-02-24 17:01:18 +0300

    Create hesap.js

---
+++ hesap.js
@@ -0,0 +1,39 @@
+function calculateOverProb(aGoals, bGoals, aMatches, bMatches) {
+  const aAvgGoals = aGoals / aMatches;
+  const bAvgGoals = bGoals / bMatches;
+  const totalAvgGoals = (aGoals + bGoals) / (aMatches + bMatches);
+  const poissonA = poisson(aAvgGoals, totalAvgGoals);
+  const poissonB = poisson(bAvgGoals, totalAvgGoals);
+  let overProb = 0;
+  for (let i = 0; i <= 10; i++) {
+    for (let j = 0; j <= 10; j++) {
+      if (i + j > 2.5) {
+        overProb += poissonA[i] * poissonB[j];
+      }
+    }
+  }
+  return overProb;
+}
+
+function poisson(avgGoals, x) {
+  const poissonArr = [];
+  for (let i = 0; i <= 10; i++) {
+    poissonArr[i] = Math.pow(Math.E, -avgGoals) * Math.pow(avgGoals, i) / factorial(i);
+  }
+  return poissonArr;
+}
+
+function factorial(n) {
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+// Test etmek için örnek veriler kullanalım
+const aGoals = 15;
+const bGoals = 10;
+const aMatches = 8;
+const bMatches = 6;
+const overProb = calculateOverProb(aGoals, bGoals, aMatches, bMatches);
+console.log(overProb); // Örnek veriler için sonucu konsolda yazdırır
