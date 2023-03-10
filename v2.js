efadede0086d0ed41f1e51a184ee419caa6eaa0a
Orknu <orkun@yandex.com>
2023-03-10 15:41:17 +0300

    Create v2.js

---
+++ v2.js
@@ -0,0 +1,25 @@
+function calculate() {
+	var totalMatchesA = document.getElementById('total-matches-a').value;
+	var totalGoalsA = document.getElementById('total-goals-a').value;
+	var totalShotsOnTargetA = document.getElementById('total-shots-on-target-a').value;
+	var shotAccuracyA = document.getElementById('shot-accuracy-a').value;
+	var conversionRateA = document.getElementById('conversion-rate-a').value;
+
+	var totalMatchesB = document.getElementById('total-matches-b').value;
+	var totalGoalsB = document.getElementById('total-goals-b').value;
+	var totalShotsOnTargetB = document.getElementById('total-shots-on-target-b').value;
+	var shotAccuracyB = document.getElementById('shot-accuracy-b').value;
+	var conversionRateB = document.getElementById('conversion-rate-b').value;
+
+	// Algoritmayı burada uygulayın
+	var avgGoalsA = totalGoalsA / totalMatchesA;
+	var avgGoalsB = totalGoalsB / totalMatchesB;
+	var avgShotsOnTargetA = totalShotsOnTargetA / totalMatchesA;
+	var avgShotsOnTargetB = totalShotsOnTargetB / totalMatchesB;
+	var shotConversionRateA = conversionRateA / 100;
+	var shotConversionRateB = conversionRateB / 100;
+	var probability = (avgGoalsA * shotAccuracyA * shotConversionRateA + avgGoalsB * shotAccuracyB * shotConversionRateB) / (avgShotsOnTargetA + avgShotsOnTargetB);
+
+	// Sonucu HTML'de gösterin
+	document.getElementById('result').innerHTML = '<p>Maçın üst bitme ihtimali: ' + (probability * 100).toFixed(2) + '%</p>';
+}
